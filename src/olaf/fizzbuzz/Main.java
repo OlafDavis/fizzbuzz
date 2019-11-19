3ed821f7d79d2db5f3ef07c4b03533a995b16367
Olaf Davis <olaf.davis@softwire.com>
2019-11-19 15:17:25 +0000

    multiples of 7 and 11 added

--- src/olaf/fizzbuzz/Main.java
+++ src/olaf/fizzbuzz/Main.java
@@ -6,15 +6,11 @@ public class Main {
         var output = "";
 	    for(var n = 1; n <= 100; n++) {
 	        output = "";
-            if (n % 3 == 0) {
-                output += "Fizz";
-            }
-            if (n % 5 == 0) {
-                output += "Buzz";
-            }
-            if (output.equals("")) {
-                output += n;
-            }
+            if (n % 3 == 0) output += "Fizz";
+            if (n % 5 == 0) output += "Buzz";
+            if (n % 7 == 0) output += "Bang";
+            if (output.equals("")) output = Integer.toString(n);
+            if (n % 11 == 0) output = "Bong";
             System.out.println(output);
         }
     }
