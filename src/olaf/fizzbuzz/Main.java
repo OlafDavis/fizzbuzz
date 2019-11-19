a1e2da0024c23ba342d495cd08b67d46923e0e93
Olaf Davis <olaf.davis@softwire.com>
2019-11-19 15:04:08 +0000

    replaced == with .equals for string comparison

--- src/olaf/fizzbuzz/Main.java
+++ src/olaf/fizzbuzz/Main.java
@@ -3,6 +3,19 @@ package olaf.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var output = "";
+	    for(var n = 1; n <= 100; n++) {
+	        output = "";
+            if (n % 3 == 0) {
+                output += "Fizz";
+            }
+            if (n % 5 == 0) {
+                output += "Buzz";
+            }
+            if (output.equals("")) {
+                output += n;
+            }
+            System.out.println(output);
+        }
     }
 }
