c7c27db6d4f660b885f576fcbf972ceedd8e7b22
Olaf Davis <olaf.davis@softwire.com>
2019-11-19 15:47:14 +0000

    multiples of 13 and 17 added, with addString(); also input maxN

--- src/olaf/fizzbuzz/Main.java
+++ src/olaf/fizzbuzz/Main.java
@@ -1,17 +1,38 @@
 package olaf.fizzbuzz;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        var output = "";
-	    for(var n = 1; n <= 100; n++) {
+        String output;
+        Boolean isReverse;                                                                                                  // indicates reversed order, if n is mult of 17
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter highest number:");
+        int maxN = scanner.nextInt();
+        for(var n = 1; n <= maxN; n++) {
+            isReverse = (n % 17 == 0);
 	        output = "";
-            if (n % 3 == 0) output += "Fizz";
-            if (n % 5 == 0) output += "Buzz";
-            if (n % 7 == 0) output += "Bang";
-            if (output.equals("")) output = Integer.toString(n);
-            if (n % 11 == 0) output = "Bong";
-            System.out.println(output);
+            if (n % 11 == 0) {
+                if (n % 13 == 0) output = addString(output, "Fezz", isReverse);
+                output = addString(output, "Bong", isReverse);
+            } else {
+                if (n % 3 == 0) output = addString(output, "Fizz", isReverse);
+                if (n % 13 == 0) output = addString(output, "Fezz", isReverse);
+                if (n % 5 == 0) output = addString(output, "Buzz", isReverse);
+                if (n % 7 == 0) output = addString(output, "Bang", isReverse);
+                if (output.equals("")) output = Integer.toString(n);
+            };
+            System.out.println(n + ": " + output);
         }
     }
+
+    static String addString(String oldString, String stringToAdd, Boolean reverseOrder) {
+        if (reverseOrder) {                                                                                             // reverseOrder means add new string to left instead of right
+            return stringToAdd + oldString;
+        } else {
+            return oldString + stringToAdd;
+        }
+    }
+
 }
